62c751616d5198cb0aeecf5b4546ad47024f5b20
sqai <13409237330@163.com>
2023-04-27 16:43:32 +0800

    修复数据库初始化权限问题:2

--- init/mysql/init.sql
+++ init/mysql/init.sql
@@ -18,14 +18,6 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-/**
-* 如果存在数据库则删除
-* DROP DATABASE IF EXISTS nacos;
-* CREATE DATABASE nacos DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-*/
-
-
-
 -- ----------------------------
 -- Table structure for config_info
 -- ----------------------------
